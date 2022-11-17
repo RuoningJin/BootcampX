b3cabd785c0753de77be8b3cf09aabf9ddac260f
RuoningJin <ronjin2013@hotmail.com>
2022-11-16 23:22:39 -0500

    paramarized the teachers.js and students.js queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,8 @@
 const { Pool } = require('pg');
 const arg = process.argv.slice(2);
+const cohort = arg[0];
+const limit = arg[1] || 5;
+const values = [`%${cohort}%`, limit];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -8,23 +11,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT id, name, cohort_id
-FROM students
-LIMIT 5;
-`)
-  .then(res => {
-    console.log(res.rows);
-  })
-  .catch(err => console.error('query error', err.stack));
+// pool.query(`
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
+// `)
+//   .then(res => {
+//     console.log(res.rows);
+//   })
+//   .catch(err => console.error('query error', err.stack));
 
 pool.query(`
 SELECT students.id as id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${arg[0]}%'
-LIMIT ${arg[1] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,4 +1,6 @@
 const {Pool} = require('pg');
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -13,9 +15,9 @@ FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
